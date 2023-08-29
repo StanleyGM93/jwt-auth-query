f31a75fdd8e731b2a9b350a674c9af9a4998d3ea
Stanley Mair <stanleygmair@gmail.com>
2023-08-30 08:26:19 +1200

    Completed up to implementing useAuth0 hook

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="mako-stanley.au.auth0.com"
+    clientId="DfF1qYBZBD7qcNlpL36UxfiWAegvHuEq"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
