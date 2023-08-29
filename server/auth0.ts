0afbb554206dfee6e3bdc0a996ce925b98b8a371
Stanley Mair <stanleygmair@gmail.com>
2023-08-30 08:53:06 +1200

    Fixed spelling error

--- server/auth0.ts
+++ server/auth0.ts
@@ -5,7 +5,7 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://mako-stanley.au.auth-.com'
+const domain = 'https://mako-stanley.au.auth0.com'
 const audience = 'https://fruits/api'
 
 const checkJwt = jwt({
